4be52bae13073ecffd13da2398dd2320a2a15c78
Chris Klemz <crklemz@gmail.com>
2021-05-28 21:56:37 -0500

    basic render DOM and html setup. working on connecting server, router, and pool but throwing error

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,4 +3,52 @@ console.log('js');
 $( document ).ready( function(){
     console.log( 'JQ' );
     
+//click listeners
+$('#submitBtn').on('click', submitTask);
+
+
+});//end document ready
+
+//submit new task to list
+function submitTask(newTask) {
+  console.log('in submitTask, new task is -->', newTask);
+ //post 
+  
+
+};//end submitTask
+
+function renderDOM(taskArray) {
+  console.log('in renderDOM');
+  //clear table before printing current array
+  $('#taskTable').empty();
+
+  //for loop to append tasks to DOM along with complete and delete buttons
+  for(let i = 0; i < taskArray.length; i++) {
+    let task = taskArray[i];
+
+    $('#taskTable').append(`
+      <tr>
+        <td>${task.task}</td>
+        <td>${task.date}</td>
+        <td>${task.complete}</td>
+        <td><button class="completeBtn" data-id="${task.id}" data-complete="${task.complete}">Task Completed</button></td>
+        <td><button class="deleteBtn" data-id="${task.id}">Delete</button></td>
+      </tr>
+    `)
+  }//end for loop
+}//end renderDOM
+
+//get the tasks from server
+function getTasks() {
+  console.log('in get tasks');
+  //ajax call to get data from server
+  $.ajax({
+    method: 'GET',
+    url: '/tasks'
+  }).then(function (response) {
+    console.log(response);
+    renderDOM(response); //runs function to print tasks to DOM
+  }).catch(function (error) {
+    console.log('error in GET on client.js', error);
   });
+}//end getTasks
---
+++ server/routes/task.router.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+const pool = require('../modules/pool');
+
+//GET
+// taskRouter.get('/', (req, res) => {
+//     let queryText = 'SELECT * FROM "tasks" ORDER BY "task";';
+//     pool.query(queryText).then(result => {
+//         res.send(result.rows);
+//     }).catch(error => {
+//         console.log('error getting tasks in router.get', error);
+//         res.sendStatus(500);
+//     });
+// });//end router.get
+
+//POST
+
+
+//PUT
+
+
+//DELETE
